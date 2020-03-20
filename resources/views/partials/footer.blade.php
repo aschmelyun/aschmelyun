e3990dfa189c6d4dfba19cdf1e8ed9a3482e35ce
Andrew Schmelyun <schmelyun@gmail.com>
2020-03-19 22:10:21 -0400

    Adds initial landing page layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,9 @@
+<footer class="px-16 mt-8 py-8">
+    <div class="flex flex-wrap justify-between items-center">
+            <p class="text-lg font-semibold">&copy;{{ date('Y') }} Andrew Schmelyun</p>
+        <p class="text-lg text-gray-700">This website was built with <a href="https://usecleaver.com" class="font-semibold text-gray-900 hover:text-red-600 transition-colors duration-200" target="_blank" rel="noreferrer">Cleaver</a>, and the entire source is on <a href="https://github.com/aschmelyun/aschmelyun" class="font-semibold text-gray-900 hover:text-red-600 transition-colors duration-200" target="_blank" rel="noreferrer">GitHub</a>.</p>
+        </div>
+    </div>
+</footer>
 <script type="text/javascript" src="{{ $mix['/assets/js/app.js'] }}"></script>
 @yield('bottom-scripts')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="px-16 py-4">
+    <div class="flex">
+        <div class="w-1/2">
+            <a class="text-2xl tracking-wider font-bold" href="/"><span class="text-red-600">A</span>SCHMELYUN</a>
+        </div>
+        <div class="w-1/2">
+            <nav>
+                <ul class="flex items-center justify-end">
+                    <li class="mr-8"><a href="#" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Projects</a></li>
+                    <li class="mr-8"><a href="#" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Work</a></li>
+                    <li class="mr-8"><a href="#" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Videos</a></li>
+                    <li class="mr-8"><a href="#" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Articles</a></li>
+                    <li><a href="#" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
