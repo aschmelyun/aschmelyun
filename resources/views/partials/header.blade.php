b7078e7812ecb11c2395b0c2ec595b3c2d6c14b1
Andrew Schmelyun <schmelyun@gmail.com>
2020-03-26 03:29:46 -0400

    Adjusts header items for mobile

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,10 +5,10 @@
         </div>
         <div class="w-full md:w-1/2 pt-2 md:pt-0">
             <nav>
-                <ul class="flex items-center justify-end">
-                    <li class="mr-8"><a href="/projects" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Projects</a></li>
-                    <li class="mr-8"><a href="https://youtube.com/aschmelyun" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200" target="_blank" rel="noreferrer">Videos</a></li>
-                    <li class="mr-8"><a href="/articles" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Articles</a></li>
+                <ul class="flex items-center justify-between md:justify-end px-6 md:px-0">
+                    <li class="mr-0 md:mr-8"><a href="/projects" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Projects</a></li>
+                    <li class="mr-0 md:mr-8"><a href="https://youtube.com/aschmelyun" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200" target="_blank" rel="noreferrer">Videos</a></li>
+                    <li class="mr-0 md:mr-8"><a href="/articles" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Articles</a></li>
                     <li><a href="mailto:me@aschmelyun.com" class="text-lg font-semibold py-1 hover:text-red-600 transition-color duration-200">Contact</a></li>
                 </ul>
             </nav>
