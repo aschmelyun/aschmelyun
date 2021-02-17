537f1fd685620fe542bc5577ffbd677b186615a9
Andrew Schmelyun <schmelyun@gmail.com>
2021-02-17 00:18:06 -0500

    Modifies nav contact link, adds links to video titles

--- resources/views/layout/videos.blade.php
+++ resources/views/layout/videos.blade.php
@@ -19,7 +19,7 @@
                 </a>
             </div>
             <div class="sm:w-3/5 mb-8 px-4">
-                <h2 class="text-2xl title-font font-medium text-gray-900 my-2">{{ $video->title }}</h2>
+                <h2 class="text-2xl title-font font-medium text-gray-900 my-2"><a href="https://youtube.com/watch?v={{ $video->id }}" class="inline-block hover:underline">{{ $video->title }}</a></h2>
                 <p class="text-gray-600 leading-relaxed mb-4"><time class="text-gray-900 font-medium" datetime="{{ date('Y-m-d H:i:s', strtotime($video->published)) }}">{{ $video->published }}</time> &mdash; {{ $video->description }}</p>
                 <a href="https://youtube.com/watch?v={{ $video->id }}" target="_blank" rel="noreferrer" class="text-gray-900 inline-flex items-center hover:underline">Watch Now
                     <svg class="w-4 h-4 ml-1" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -9,10 +9,10 @@
             <a href="/uses" class="mr-0 lg:mr-8 hover:text-gray-600 transition-colors duration-200">Uses</a>
             <a href="/projects" class="mr-0 lg:mr-8 hover:text-gray-600 transition-colors duration-200">Projects</a>
         </nav>
-        <button onclick="window.location = '/contact'" class="inline-flex items-center bg-gray-200 text-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-900 hover:text-white rounded font-semibold text-lg mt-4 md:mt-0 transition-colors duration-200">Get In Touch
+        <a href="mailto:me@aschmelyun.com?subject=Hey Andrew" class="inline-flex items-center bg-gray-200 text-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-900 hover:text-white rounded font-semibold text-lg mt-4 md:mt-0 transition-colors duration-200">Get In Touch
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
-        </button>
+        </a>
     </div>
 </header>
